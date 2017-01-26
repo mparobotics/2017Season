cda6ec4744f1fccdbd1c456a42a5b85172548ce8
Brookshank <jbrooksbank17@moundsparkacademy.org>
2017-01-26 17:24:33 -0600

    mhm

--- RasberryPi/GripRunner.py
+++ RasberryPi/GripRunner.py
@@ -45,7 +45,7 @@ def main():
         if have_frame:
             pipeline.process(frame)
             extra_processing(pipeline)
-            (cv2.drawContours(frame, pipeline.filter_contours_output, -1, (255, 255, 255), thickness="CV_FILLED",
+            (cv2.drawContours(frame, pipeline.filter_contours_output, -1, (255, 255, 255), thickness=-1
                               ))
             cv2.imwrite('pic.jpg', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
