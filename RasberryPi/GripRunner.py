9663d1e352145964534dde71103f4afc42a38da4
brookshank <jbrooksbank17@moundsparkacademy.org>
2017-01-31 23:46:05 +0000

    got labeling system working

--- RasberryPi/GripRunner.py
+++ RasberryPi/GripRunner.py
@@ -5,6 +5,8 @@ import cv2
 from networktables import NetworkTables
 from grip import GripPythonVI  # TODO change the module and class, if needed
 
+image_scale = 0.5
+
 
 def extra_processing(pipeline):
     """
@@ -18,13 +20,12 @@ def extra_processing(pipeline):
     heights = []
     areas = []
 
-    
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
         center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
-        center_y_positions.append(y + h / w)
+        center_y_positions.append(y + h / 2)
         widths.append(w)
-        heights.append(y)
+        heights.append(h)
         areas.append(w * h)
 
     table = NetworkTables.getTable('/vision/high_goal')
@@ -33,6 +34,18 @@ def extra_processing(pipeline):
     table.putNumberArray('width', widths)
     table.putNumberArray('height', heights)
     table.putNumberArray('area', areas)
+
+
+def draw_contours(pipeline, frame):
+    contour_number = 0
+    contour_frame = cv2.resize(frame, (0, 0), fx=image_scale, fy=image_scale)
+    for contour in pipeline.filter_contours_output:
+        x, y, w, h = cv2.boundingRect(contour)
+        center = (x+(w/2)), (y+(h/2))
+        (cv2.drawContours(contour_frame, pipeline.filter_contours_output, -1, (255, 0, 120), cv2.FILLED))
+        cv2.putText(contour_frame, str(contour_number), center, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        contour_number += 1
+    return contour_frame
 
 
 def main():
@@ -46,17 +59,8 @@ def main():
         if have_frame:
             pipeline.process(frame)
             extra_processing(pipeline)
-            resized_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-            (cv2.drawContours(resized_frame, pipeline.filter_contours_output,
-                              -1, (255, 0, 120), thickness=-1))
-            for contour in pipeline.filter_contours_output:
-                x, y, w, h = cv2.boundingRect(contour)
-                center = ((x+w)/2), ((y+h)/2)
-                cv2.putText(resized_frame, contour_number, center, "FONT_HERSHEY_PLAIN", 9, (255, 255, 255))
-                contour_number += 1
-            cv2.imwrite('/home/pi/git/2017Season/RasberryPi/pic.jpg', resized_frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            cv2.imwrite('/home/pi/git/2017Season/RasberryPi/pic.jpg', draw_contours(pipeline, frame))
+
 
     print('Stopped Capturing')
 
