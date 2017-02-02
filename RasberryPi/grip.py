3850687ba70479197404fe6dbf29596439142366
Brookshank <jbrooksbank17@moundsparkacademy.org>
2017-02-02 15:48:57 -0600

    finished deploy script

--- RasberryPi/grip.py
+++ RasberryPi/grip.py
@@ -1,7 +1,5 @@
 import cv2
-import numpy
-import math
-from enum import Enum
+
 
 
 class GripPythonVI:
--- utilities/deploy.sh
+++ utilities/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp grip.pi pi@grippi.local:/home/pi/git/2017Season/RasberryPi/
-ssh pi@grippi.local cd /git/2017Season/RasberryPi && workon cv && python GripPython.py
+scp grip.py pi@grippi.local:/home/pi/git/2017Season/RasberryPi/
+ssh pi@grippi.local sudo deploy_grip
