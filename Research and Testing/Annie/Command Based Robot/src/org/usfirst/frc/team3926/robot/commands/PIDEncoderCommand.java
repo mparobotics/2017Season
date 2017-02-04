88801385dad74dd5aeeac510dae21e7e4aaac8ce
APortoghese <aportoghese20@moundsparkacademy.org>
2017-02-04 13:46:44 -0600

    PID stuff

---
+++ Research and Testing/Annie/Command Based Robot/src/org/usfirst/frc/team3926/robot/commands/PIDEncoderCommand.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team3926.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team3926.robot.Robot;
+
+/**
+ * keeps the motor at a constant preset speed
+ */
+public class PIDEncoderCommand extends Command {
+
+    /**
+     * tells code it needs to access things in the climbing subsystem
+     */
+    public PIDEncoderCommand() {
+
+        requires(Robot.PIDencoder);
+
+    }
+
+    /**
+     * enables the PID loop
+     */
+    protected void initialize() {
+
+        Robot.PIDencoder.enable();
+
+    }
+
+    /**
+     * nothing needed
+     */
+    protected void execute() {
+
+
+    }
+
+    /**
+     * should stay running
+     * @return
+     */
+    protected boolean isFinished() {
+
+        return false;
+    }
+
+    /**
+     * disables the PID loop
+     */
+    protected void end() {
+
+        Robot.PIDencoder.disable();
+
+    }
+
+    /**
+     * nothing needed
+     */
+    protected void interrupted() {
+
+    }
+}
