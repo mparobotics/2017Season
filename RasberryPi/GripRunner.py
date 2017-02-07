108952856d3dbf733b33d223f9646d230dbf8438
brookshank <jbrooksbank17@moundsparkacademy.org>
2017-02-07 23:27:45 +0000

    Commiting from Pi

--- RasberryPi/GripRunner.py
+++ RasberryPi/GripRunner.py
@@ -1,6 +1,6 @@
 #! /home/pi/.virtualenvs/cv/bin/python2
 
-# import cv2.cv2 as cv2
+#import cv2.cv2 as cv2
 import cv2
 from networktables import NetworkTables
 from grip import GripPythonVI  # TODO change the module and class, if needed
@@ -8,32 +8,37 @@ from grip import GripPythonVI  # TODO change the module and class, if needed
 image_scale = 0.5
 
 
-def extra_processing(pipeline):
+def extra_processing(pipeline, frame):
     """
     Performs extra processing on the pipeline's outputs and publishes data to NetworkTables.
     :param pipeline: the pipeline that just processed an image
+    :param frame: the current frame being processed
     :return: None
     """
     center_x_positions = []
     center_y_positions = []
     widths = []
     heights = []
-    areas = []
-
+    contour_number = 0
+    contour_frame = cv2.resize(frame, (0, 0), fx=image_scale, fy=image_scale)
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
         center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
         center_y_positions.append(y + h / 2)
         widths.append(w)
         heights.append(h)
-        areas.append(w * h)
+        draw_center = x + (w/2), y + (h/2)
+        (cv2.drawContours(contour_frame, contour, -1, (255, 0, 120), cv2.FILLED))
+        cv2.putText(contour_frame, str(contour_number), draw_center, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        contour_number += 1
 
     table = NetworkTables.getTable('/vision/high_goal')
     table.putNumberArray('x', center_x_positions)
     table.putNumberArray('y', center_y_positions)
     table.putNumberArray('width', widths)
     table.putNumberArray('height', heights)
-    table.putNumberArray('area', areas)
+    print(contour_number)
+    return contour_frame
 
 
 def draw_contours(pipeline, frame):  # TODO combine this with extra_processing
@@ -45,9 +50,10 @@ def draw_contours(pipeline, frame):  # TODO combine this with extra_processing
     """
     contour_number = 0
     contour_frame = cv2.resize(frame, (0, 0), fx=image_scale, fy=image_scale)
+    number_of_contours = len(pipeline.filter_contours_output)
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
-        center = (x + (w / 2)), (y + (h / 2))
+        center = x, y
         (cv2.drawContours(contour_frame, pipeline.filter_contours_output, -1, (255, 0, 120), cv2.FILLED))
         cv2.putText(contour_frame, str(contour_number), center, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
         contour_number += 1
@@ -69,8 +75,8 @@ def main():  # TODO debug grip class to see why it does not see some contours
         have_frame, frame = cap.read()
         if have_frame:
             pipeline.process(frame)
-            extra_processing(pipeline)
-            cv2.imwrite('/home/pi/git/2017Season/RasberryPi/pic.jpg', draw_contours(pipeline, frame))
+            # extra_processing(pipeline)
+            cv2.imwrite('/home/pi/git/2017Season/RasberryPi/pic.jpg', extra_processing(pipeline, frame))
 
     print('Stopped Capturing')
 
--- utilities/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp grip.py pi@grippi.local:/home/pi/git/2017Season/RasberryPi/
-ssh pi@grippi.local sudo deploy_grip
